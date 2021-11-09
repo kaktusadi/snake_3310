522f3a8ba1034608b1f0fd8834efdae613fd9978
kaktusadi <adrian.sienczak@gmail.com>
2021-11-09 23:23:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ while game_flag:
         scoreboard.increase_score()
 
     #Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() > -280 or snake.head.ycor() > 280 or snake.head.ycor() > -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_flag = False
         scoreboard.game_over()
 
